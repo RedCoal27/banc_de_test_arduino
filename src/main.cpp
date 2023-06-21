4ca79adb83dceed0601603c56dec4654c54f2ae3
RedCoal27 <alxandre.toupie@gmail.com>
2023-06-21 10:20:34 +0200

    class mcp modif main+ déplacement define

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,26 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <mcp.h>
 
+mcp mcp1(0x20);
+mcp mcp2(0x21);
 
-#define chipAddrIn = 0x20;
-#define chipAddrOut = 0x27;
-#define GPIOA = 0x12; // PortA reg addr
-#define GPIOB = 0x13; // PortB reg addr
-#define IODIRA = 0x00; // PortA direction reg addr
-#define IODIRB = 0x01; // PortB direction reg addr                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                void setup() {
-#define INTCONA = 0x08;
-#define INTFA = 0x0e;
-#define INTCAPA = 0x10;
-#define IOCON = 0x0a;
-#define GPINTENA = 0x04;
-#define IPOLA = 0x02;
-#define DEFVALA = 0x06;
-#define GPPUA = 0x0c;
-#define GPPUB = 0x0d;
-
-
-int a =0; 
+void setup() {
+    mcp1.begin();
+    mcp1.setupPortA(0x00, 0x0f, 0xff);
+    mcp1.setupPortB(0x00, 0x0f, 0xff);
     
-byte pin0 ;
-byte pin1 ;  
-
-  byte AOut;
-  
-void mcu_write(int Addr,int registre,int value){
-    Wire.beginTransmission(Addr);
-    Wire.write(registre); // IOCON dir register
-    Wire.write(value); // 
-    Wire.endTransmission();
+    mcp2.begin();
+    mcp2.setupPortA(0x00, 0x00, 0x00);
+    mcp2.setupPortB(0x00, 0x00, 0x00);
 }
 
-
-  void setup()
-  {
-  Wire.begin();
-
-  Serial.begin(9600);
-  mcu_write(chipAddrIn,GPPUA,0x0f);
-  mcu_write(chipAddrIn,IODIRA,0xff);//input
-  mcu_write(chipAddrIn,IPOLA, 0x00);//polarité 
-  
-  mcu_write(chipAddrOut,IODIRA,0x00);//output
- 
-  }
-
-  
-  void loop()
-  {
-  //pinMode( 13, OUTPUT );
-  //mcpIn.pinMode(0, OUTPUT);
-  //digitalWrite(13,LOW);
-  //mcpIn.digitalWrite(0,LOW);
-  //delay(1000);
-  //digitalWrite(13,HIGH);
-  //mcpIn.digitalWrite(0,HIGH);
-  delay(3000);
-  
-
-
-  Wire.beginTransmission(chipAddrIn);
-  Wire.write(GPIOA);
-  Wire.endTransmission();
-  delay(10);
-  Wire.requestFrom(chipAddrIn,1); // IOCON dir register
-  if(Wire.available()){
-    pin0 = Wire.read();
-  
+void loop() {
+    byte pinA = mcp1.readGPIO(GPIOA);
+    byte pinB = mcp1.readGPIO(GPIOB);
+    Serial.println(pinA, BIN);
+    Serial.println(pinB, BIN);
     
-    //pin0 = mcpIn.digitalRead(0);
-    //pin1 = mcpIn.digitalRead(1);
-    Serial.print("Digital Input ");
-    Serial.println(pin0,BIN);
-    //Serial.print("Digital Input 1 --> ");
-    //Serial.println(pin1,BIN);
-    }
-
-  AOut = !AOut;
-  Serial.println(AOut);
-  mcu_write(chipAddrOut,GPIOA,AOut);
-
-  }
+    mcp2.writeGPIO(GPIOA, !pinA);
+    mcp2.writeGPIO(GPIOB, !pinB);
+    delay(3000);
+}
--- src/mcp.h
+++ src/mcp.h
@@ -4,6 +4,18 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+
+#define GPIOA 0x12 // PortA reg addr
+#define GPIOB 0x13 // PortB reg addr
+#define IODIRA 0x00 // PortA direction reg addr
+#define IODIRB 0x01 // PortB direction reg addr
+#define IPOLA 0x02 // PortA polarity register
+#define IPOLB 0x03 // PortB polarity register
+#define GPPUA 0x0c // PortA pull-up resistor configuration
+#define GPPUB 0x0d // PortB pull-up resistor configuration
+
+
+
 class mcp {
     public:
         mcp(uint8_t chipAddr);
